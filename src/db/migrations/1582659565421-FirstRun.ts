84fed00f8bc4814656ff3012c505ba6dba6ebd7b
Mateus Garcia <mateus.gigainfo@gmail.com>
2020-02-25 17:32:35 -0300

    migrations

--- src/db/migrations/1582659565421-FirstRun.ts
+++ src/db/migrations/1582659565421-FirstRun.ts
@@ -11,24 +11,34 @@ export class CreatePermissions1582659565421 implements MigrationInterface {
 
         ///////////////////////////////////////////////////////////
         let p1 = new Permission();
+        p1.id = 1;
         p1.operation = 'list';
         p1.feature = 'admin';
         p1 = await connection.getRepository( Permission ).save( p1 );
         ///////////////////////////////////////////////////////////
         let p2 = new Permission();
+        p2.id = 2;
         p2.operation = 'edit';
         p2.feature = 'admin';
         p2 = await connection.getRepository( Permission ).save( p2 );
         ///////////////////////////////////////////////////////////
         let p3 = new Permission();
+        p3.id = 3;
         p3.operation = 'delete';
         p3.feature = 'admin';
         p3 = await connection.getRepository( Permission ).save( p3 );
         ///////////////////////////////////////////////////////////
         let p4 = new Permission();
-        p4.operation = 'list';
-        p4.feature = 'log';
+        p4.id = 4;
+        p4.operation = 'create';
+        p4.feature = 'admin';
         p4 = await connection.getRepository( Permission ).save( p4 );
+        ///////////////////////////////////////////////////////////
+        let p5 = new Permission();
+        p5.id = 5;
+        p5.operation = 'list';
+        p5.feature = 'log';
+        p5 = await connection.getRepository( Permission ).save( p5 );
         ///////////////////////////////////////////////////////////
         ///////////////////////////////////////////////////////////
         ///////////////////////////////////////////////////////////
@@ -44,13 +54,15 @@ export class CreatePermissions1582659565421 implements MigrationInterface {
 
         ///////////////////////////////////////////////////////////
         let r1 = new Role()
-        r1.permissions = [ p1, p2, p3, p4 ];
+        r1.id = 1;
+        r1.permissions = [ p1, p2, p3, p4, p5 ];
         r1.name = "Master"
         r1.description = "Role Master padrão. Concede todas as permissões.";
         r1 = await connection.getRepository( Role ).save( r1 );
         ///////////////////////////////////////////////////////////
         let r2 = new Role();
-        r2.permissions = [ p1, p4 ];
+        r2.id = 2;
+        r2.permissions = [ p1, p5 ];
         r2.name = "Auditoria"
         r2.description = "Role para auditoria, concede todos os direitos de visualização";
         r2 = await connection.getRepository( Role ).save( r2 );
@@ -70,6 +82,7 @@ export class CreatePermissions1582659565421 implements MigrationInterface {
 
         ///////////////////////////////////////////////////////////
         let ad1 = new Admin();
+        ad1.id = 1;
         ad1.birthDay = new Date();
         ad1.cpf = '12345678900';
         ad1.email = 'adminmaster@server.com'
